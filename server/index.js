ca64ada1abdbfaaacef3323d5ab7eeb5752838db
Russo3007 <russoyoav@gmail.com>
2023-09-20 21:44:48 +0300

    fixed path to routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/routes.js');
+const routes = require('./src/routes/routes.js');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
