26643ad9787642b7dc0a251a16fcb37c553f53d4
Russo3007 <russoyoav@gmail.com>
2023-09-26 04:35:23 +0300

    changed code blocks data

--- server/src/models/codeBlocks.js
+++ server/src/models/codeBlocks.js
@@ -2,7 +2,7 @@ const codeBlocks = [
     {
       title: 'Async case',
       id: 'fb1e3ee093a843d8b3d57cf50cb10bfc',
-      code: 'async function fetchData() {\n  const response = await fetch(\'https://api.example.com/data\');\n  const data = await response.json();\n  console.log(data);\n}',
+      code: 'async function fetchData() {\n  // Write your code here \n  console.log(data);\n}',
       solution: 'async function fetchData() {\n  const response = await fetch(\'https://api.example.com/data\');\n  const data = await response.json();\n  console.log(data);\n}'
     },
     {
@@ -10,6 +10,12 @@ const codeBlocks = [
       id: '4954713ee4c0479594c96ffbd16b0299',
       code: 'fetch(\'https://api.example.com/data\')\n  .then(response => response.json())\n  .then(data => console.log(data))\n  .catch(error => console.error(error));',
       solution: 'fetch(\'https://api.example.com/data\')\n  .then(response => response.json())\n  .then(data => console.log(data))\n  .catch(error => console.error(error));'
+    },
+    {
+      title: 'Array Filtering',
+      id: '9cfb3cfa609b4c12b7310fdv4cag3554x8e3',
+      code: 'const numbers = [1, 2, 3, 4, 5];\nconst evenNumbers = numbers.filter(num => {\n  // Write your code here to filter even numbers\n});\nconsole.log(evenNumbers);',
+      solution: 'const numbers = [1, 2, 3, 4, 5];\nconst evenNumbers = numbers.filter(num => {\n  return num % 2 === 0;\n});\nconsole.log(evenNumbers);',
     },
     {
       title: 'For Loop',
@@ -28,6 +34,12 @@ const codeBlocks = [
       id: '6d8b7c02d1c44e55abec032bba3dabfa',
       code: 'const person = {\n  firstName: "John",\n  lastName: "Doe",\n  age: 30\n};\nconsole.log(person.firstName, person.age);',
       solution: 'const person = {\n  firstName: "John",\n  lastName: "Doe",\n  age: 30\n};\nconsole.log(person.firstName, person.age);'
+    },
+    {
+      title: 'Object Destructuring',
+      id: 'abcdef123fj4if3ddsoe3pspdk30clst95gk',
+      code: 'const person = {\n  name: \'Alice\',\n  age: 30,\n  city: \'New York\'\n};\n// Use object destructuring to extract the name\n// Write your code here\nconsole.log(name);',
+      solution: 'const person = {\n  name: \'Alice\',\n  age: 30,\n  city: \'New York\'\n};\n// Use object destructuring to extract the name\nconst { name } = person;\nconsole.log(name);',
     },
     // Add more code blocks here
   ];
