dfe0aa1e518cfa59c138f0dda39e4eb0cc17a491
Russo3007 <russoyoav@gmail.com>
2023-10-01 20:07:18 +0300

    added MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
 const { Server } = require("socket.io");
 const codeBlocks = require("./src/models/codeBlocks")
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://admin:ghiW09ZbDYC5j7ho@codeblockcluster.foyt3e3.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true });
 require('dotenv').config();
+
+// Connect to the MongoDB server
+client.connect((err) => {
+  if (err) {
+    console.error('MongoDB connection error:', err);
+  } else {
+    console.log('Connected to MongoDB');
+  }
+});
+
+const db = client.db('CodeBlockCluster');
+const codeBlocksCollection = db.collection('codeBlocks');
+
+// Insert code blocks data into the MongoDB collection
+codeBlocksCollection.insertMany(codeBlocks, (err, result) => {
+  if (err) {
+    console.error('Error inserting data into MongoDB:', err);
+  } else {
+    console.log('Inserted', result.insertedCount, 'documents into MongoDB');
+  }
+});
 
 const io = new Server(process.env.PORT, {
   cors: {
